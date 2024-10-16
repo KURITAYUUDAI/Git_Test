e334153a3cbcb607bd4e883eba35c575af000c58
ZISYO <yuudai.klt@gmail.com>
2024-10-16 12:43:08 +0900

    なんか変更

--- Git_Test_Project.cpp
+++ Git_Test_Project.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello VisualStudio Git!\n";
 
-    std::cout << "Conflict changed!\n";
+    std::cout << "なんか変更\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
